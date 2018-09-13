e247b282c3777c92e5307facacd5aef41036aaf4
aucookson97 <aidanucookson@gmail.com>
2018-09-13 14:34:22 -0400

    Reads CSV, Creates Board States

---
+++ decisionTree.py
@@ -0,0 +1,46 @@
+import csv
+
+class BoardState():
+
+
+    def __init__(self, board1D):
+        self.winner = board1D[len(board1D)-1]
+        self.rows = 6
+        self.columns = 7
+
+        self.board = []
+
+        for c in range(self.columns):
+            self.board.append([])
+            for r in range(self.rows):
+                index = c * self.rows + (self.rows - r - 1)
+                self.board[c].append(board1D[index])
+
+    def display(self):
+        print ""
+        for r in range(self.rows):
+            line = ""
+            for c in range(self.columns):     
+                line += self.board[c][r] + " "
+            print line
+        print "\n\tWinner: {}".format(self.winner)
+    
+
+
+def loadTrainData(filename):
+    data = []
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        reader_iter = iter(reader)
+        reader_iter.next()
+        for entry in reader_iter:
+            data.append(BoardState(entry))
+    return data
+
+if __name__== "__main__":
+    data = loadTrainData("trainDataSet.csv")
+
+    print len(data)
+    #for i in range(10):
+     #   data[i].display()
