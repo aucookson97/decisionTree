b2d266aaad9f3ec9823d44a18e61d43c0b7fc4e9
aucookson97 <aidanucookson@gmail.com>
2018-09-14 14:45:45 -0400

    Added decision data converter method

--- decisionTree.py
+++ decisionTree.py
@@ -20,7 +20,7 @@ class FeatureFinder():
                     player2count += 1
         return player1count - player2count
 
-    def centerPiece(selfself, state):
+    def centerPiece(self, state):
         return state.board[3][0]
 
 class BoardState():
@@ -60,6 +60,20 @@ def loadTrainData(filename):
             data.append(BoardState(entry))
     return data
 
+def createDecisionData(finder, dataSet):
+    decision_data = []
+    winner_data = []
+
+    for i in range(len(dataSet)):
+        data = dataSet[i]
+        decision_data.append([])
+        feature_1 = finder.bottomLeft(data)
+        feature_2 = finder.numCenterPieces(data)
+        feature_3 = finder.centerPiece(data)
+        decision_data[i].extend((feature_1, feature_2, feature_3))
+        winner_data.append(data.winner)
+    return (decision_data, winner_data)
+
 if __name__== "__main__":
     data = []
     try:
@@ -72,7 +86,9 @@ if __name__== "__main__":
 
     finder = FeatureFinder()
 
-    data[19].display()
-    print finder.bottomLeft(data[0])
-    print finder.numCenterPieces(data[19])
-    print finder.centerPiece(data[19])
+    decision_data, winner_data = createDecisionData(finder, data)
+
+    data[0].display()
+
+    print decision_data[0]
+    print winner_data[0]
